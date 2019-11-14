b02514e403f9c28a4fe502e76754947583a62809
oredola <oredolaolamide@gmail.com>
2019-11-14 15:51:49 +0100

    Added form validation and error messaging

--- app/src/Components/UserForm.jsx
+++ app/src/Components/UserForm.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { withFormik, Form, Field } from "formik";
+import { withFormik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from 'yup';
 
 function UserForm(props) {
 
@@ -10,10 +11,14 @@ function UserForm(props) {
           Name
           <Field name="name" type="text" placeholder="Input your name" />
         </label>
+        <ErrorMessage name="name">{msg => <div>{msg}</div>}</ErrorMessage>
+
         <label>
           Email
           <Field name="email" type="email" placeholder="Pick an email" />
         </label>
+        <ErrorMessage name="email">{msg => <div>{msg}</div>}</ErrorMessage>
+
         <label>
           Password
           <Field
@@ -22,10 +27,13 @@ function UserForm(props) {
             placeholder="Input your password"
           />
         </label>
+        <ErrorMessage name="password">{msg => <div>{msg}</div>}</ErrorMessage>
+
         <label>
           Terms of Service
           <Field name="tos" type="checkbox" placeholder="Agree with us?" />
         </label>
+        <ErrorMessage name="tos">{msg => <div>{msg}</div>}</ErrorMessage>
 
         <button type="submit">Submit</button>
       </Form>
@@ -39,13 +47,28 @@ const FormWithFormik = withFormik({
 			name: "",
 			email: "",
 			password: "",
-			tos: 'checked'
+			tos: false
 		}
 	},
 
+	validationSchema: Yup.object().shape({
+		name: Yup.string()
+			.min(2, 'Is that even a name')
+			.required("Name can not be blank"),
+		email: Yup.string()
+			.email('Valid email or nothing')
+			.required("How do we send you goodies without an address?"),
+		password: Yup.string()
+			.min(5, 'Hackers are not playing. So should you!')
+			.required("Safeguard your account like you mean business"),
+		tos: Yup
+			.boolean()
+			.required("We can't roll if you don't agree to our terms")
+	}),
+
 	handleSubmit(values, tools) {
 		return null
-	}
+	}, 
 
 })(UserForm);
 
